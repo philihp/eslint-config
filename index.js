11fd49d3d7ee6a8b1d3ecded1563cdf089982d92
Philihp Busby <philihp@gmail.com>
2020-06-18 23:30:50 +0000

    add eslint-unicorn -- this is probably really breaking

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: [
-    'airbnb',
-    'airbnb/hooks',
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:prettier/recommended',
     'plugin:json/recommended',
+    'plugin:unicorn/all',
+    'plugin:jest/all',
+    // trust in jordan on everything, except he's wrong about semi
+    'airbnb',
+    'airbnb/hooks',
+    // always put prettier last to avoid thrashing conflicts
     'prettier/react',
+    'plugin:prettier/recommended',
   ],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['setupTests.js', '**/__tests__/*.test.js'],
+      },
+    ],
   },
 }
