79e118b5f5468d108412d2fd1f3e4c522f761b09
Philihp Busby <philihp@gmail.com>
2022-08-01 18:24:35 +0000

    fix some rules i've found annoying

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ module.exports = {
     ],
     'react/jsx-filename-extension': 'off',
     'import/no-default-export': 'off', // things like next.js need this for components, but otherwise it would be a warn
+    'import/extensions': [
+      // typescript
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
     'import/no-extraneous-dependencies': [
       'error',
       {
@@ -31,10 +40,15 @@ module.exports = {
       },
     ],
     'import/prefer-default-export': 'off', // https://stackoverflow.com/a/68665805/643928
+    'jsx-a11y/anchor-is-valid': 0,
+    'no-bitwise': 'off', // but what if you want to use bitwise operators?
+    'no-plusplus': 'off', // but what if you want to use the ++ operator?
     'no-unused-vars': [
       'error',
       {
         argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
       },
     ],
     'no-shadow': 'off', // this can result in cleaner code, and is mitigated by decent testing
